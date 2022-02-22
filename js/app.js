81d6cfc7bd95c62e9ae1b6d1261d62cf79e868dd
Manikbr1 <97015482+Manikbr1@users.noreply.github.com>
2022-02-22 09:30:40 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function (){
+        $('#hideBtn').click(function() {
+            $('#know').hide();
+        })
+    
+        $('#showBtn').click(function() {
+            $('#know').show();
+        })
+        
+        $('#toggleBtn').click(function() {
+            $('#know').toggle('slow');
+        })
+        
+    
+        $('#maltitypeBtn').mouseenter(function() {
+            $('#know').hide(3000);
+        })
+        $('#maltitypeBtn').mouseleave(function() {
+            $('#know').show(3000);
+        })
+    
+        $('#fadeOut').click(function() {
+            $('#output').fadeOut(4000);
+        })
+        $('#fadeIn').click(function() {
+            $('#output').fadeIn(4000);
+        })
+    
+        $('#fadeToggle').click(function() {
+            $('#output').fadeToggle(3000);
+        })
+    
+        $('#fadeTo').click(function() {
+            $('#output').fadeTo(1000, .2);
+        })
+    
+    
+        $('#animate').click(function() {
+            $('#output').animate({
+                width: '600px'
+            },5000);
+        })
+        $('#animateOf').click(function() {
+            $('#output').stop();
+        })
+        
+    
+        $('#mathChain').click(function() {
+            $('#output').hide(2000).fadeIn(4000).animate({
+                width:'50%'
+            },6000);
+        })
+    })
